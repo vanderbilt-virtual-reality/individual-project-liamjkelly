9eec8656de41c35a3921e783087996a23309184f
liamjkelly <liamjkelly11@gmail.com>
2019-11-22 15:20:59 -0600

    Fix exe folder name, attempt to fix jumpyness of bottle

---
+++ Assets/MakeBottleChild.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeBottleChild : MonoBehaviour
+{
+    GameObject grabbed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        grabbed = gameObject.GetComponent<OVRGrabber>().grabbedObject;
+        if (grabbed != null)
+        {
+            grabbed.transform.parent = gameObject.transform;
+        }
+    }
+}
